3ad6b8c2708c48d47647b6d932679473fccc635a
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-08-31 12:23:25 -0400

    heroku deploy ran build

---
+++ dist/models/userModel.js
@@ -0,0 +1,41 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+/* User Schema */
+var userSchema = new _mongoose["default"].Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    dropDups: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    "default": false
+  }
+});
+/* Model */
+
+var userModel = _mongoose["default"].model("User", userSchema);
+
+var _default = userModel;
+/* dropDups: true    note this is deprecated*/
+
+exports["default"] = _default;
